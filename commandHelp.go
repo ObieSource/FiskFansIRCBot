a30d9c48e4c1731c8039f1f41e9d851268e3b406
William Rehwinkel <willrehwinkel@gmail.com>
2022-04-06 15:27:54 -0400

    disable fancy formtting for help

--- commandHelp.go
+++ commandHelp.go
@@ -3,18 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"strings"
 )
 
 func HelpHandler(argv []string) string {
 	buf := new(bytes.Buffer)
 
-	blankLine := "+" + strings.Repeat("-", 10) + "+" + strings.Repeat("-", 17) + "+" + strings.Repeat("-", 48) + "+"
-
 	for _, c := range Commands {
-		fmt.Fprintln(buf, blankLine)
-		fmt.Fprintf(buf, "| %-8s | %-15s | %-46s |\n", "."+c.Command, c.Args, c.Description)
+		fmt.Fprintf(buf, "%-8s %-15s %-46s\n", "."+c.Command, c.Args, c.Description)
 	}
-	fmt.Fprintln(buf, blankLine)
 	return buf.String()
 }
