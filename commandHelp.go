ced1ddfb50e3f34afe4b9b060975deab6d64a60f
William Rehwinkel <willrehwinkel@gmail.com>
2022-04-06 01:45:47 -0400

    stylized help message

--- commandHelp.go
+++ commandHelp.go
@@ -3,13 +3,18 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 func HelpHandler(argv []string) string {
 	buf := new(bytes.Buffer)
 
+	blankLine := "+" + strings.Repeat("-", 10) + "+" + strings.Repeat("-", 17) + "+" + strings.Repeat("-", 48) + "+"
+
 	for _, c := range Commands {
-		fmt.Fprintf(buf, "%-8s%-15s%s\n", c.Command, c.Args, c.Description)
+		fmt.Fprintln(buf, blankLine)
+		fmt.Fprintf(buf, "| %-8s | %-15s | %-46s |\n", c.Command, c.Args, c.Description)
 	}
+	fmt.Fprintln(buf, blankLine)
 	return buf.String()
 }
